f766a43d7a6eef7db840dedf3863ddadc1e33fa9
Jacob Lundell <jacoblundell@hotmail.se>
2020-11-18 17:46:38 +0100

    CLS fix

--- fall20'/scaling.js
+++ fall20'/scaling.js
@@ -1,6 +1,6 @@
 function load()	{
-	var parentHeight = (document.getElementById('intro-height').clientHeight) * .8
-	document.getElementById('intro-height-applied').style.height = parentHeight + "px"
+	/* var parentHeight = (document.getElementById('intro-height').clientHeight) * .8
+	document.getElementById('intro-height-applied').style.height = parentHeight + "px" */
 
 	var parentHeight2 = (document.getElementById('content-height').clientHeight) * .9
 	document.getElementById('content-height-applied').style.height = parentHeight2 + "px"
