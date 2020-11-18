79e8b4e67f55537022d4fdb32b84ecfc56132e4b
Jacob Lundell <jacoblundell@hotmail.se>
2020-11-18 18:01:50 +0100

    Performance Update

--- fall20'/elements.js
+++
@@ -1,5 +0,0 @@
-document.createElement('p-page');
-document.createElement('p-scaled');
-document.createElement('p-scaled-parent');
-document.createElement('p-content-height');
-document.createElement('p-row');
